dc2cbbd3af55f783392b46d9635c0f2463c1e971
Veronica Barcia <veronicabarcia4@gmail.com>
2020-07-27 20:13:24 -0400

    index

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
-api.use(express.static(__dirname + '/public'));
 const api = express();
+api.use(express.static(__dirname + '/public'));
+
 api.listen(3000, () => {
   console.log('API up and running!');
 });
 
-// api.get('/', (req, res) => {
-//     console.log(req);
-//     res.send('Hello, world!');
-//   });
+api.get('/', (req, res) => {
+    console.log(req);
+    res.send('Hello, world!');
+  });
